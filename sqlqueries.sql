2b58a11d0aec0c8afd834418025dfb61f28f5498
Georges <georges.boughantous@gmail.com>
2021-04-20 19:20:32 +1000

    Update sqlqueries.sql

--- sqlqueries.sql
+++ sqlqueries.sql
@@ -11,7 +11,7 @@ NSERT INTO ISDUSER."USER" (EMAIL, "NAME", PASSWORD, PHONE, GENDER, DOB)
 	VALUES 
   	('jane.doe@uts.edu.au', 'Jane S. Doe', '12345', '99990000', 'F', '05/05/1995'),
 	('john.smith@uts.edu.au', 'John Smith', '111111', '99991111', 'M', '05/06/1990'),
-	('jim.carry@uts.edu.au', 'Jim Carry', '12333', '99912000', 'M', '06/05/1999',
+	('jim.carry@uts.edu.au', 'Jim Carry', '12333', '99912000', 'M', '06/05/1999'),
 	('julie.l@uts.edu.au', 'Julie Laveaux', '12222', '99991100', 'F', '05/11/2000'), 
 	('lucy.lu@uts.edu.au', 'Lucy Lu', '66600', '99990011', 'F', '02/01/1991');
 

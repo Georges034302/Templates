ecfd224d8dfb9c0339aa197fccc57baf9d33e46d
Georges <georges.boughantous@gmail.com>
2021-04-26 10:27:23 +1000

    Create DB.java

---
+++ DB.java
@@ -0,0 +1,15 @@
+import java.sql.Connection;
+
+/**
+ *
+ * @author George
+ */
+public abstract class DB {
+    protected String URL = "jdbc:derby://localhost:1527/";
+    protected String db = "studentsdb";
+    protected String dbuser = "isduser";
+    protected String dbpass = "admin"; 
+    protected String driver = "org.apache.derby.jdbc.ClientDriver";
+    protected Connection conn;   
+    protected String table;
+}

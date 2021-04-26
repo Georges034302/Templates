e8b328e01d60ab024429e14fb8b5d63eefc050c4
Georges <georges.boughantous@gmail.com>
2021-04-26 11:41:09 +1000

    Update DBConnetor.java

--- DBConnetor.java
+++ DBConnetor.java
@@ -1,3 +1,10 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts.isd.model.dao;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -8,20 +15,16 @@ import java.sql.SQLException;
  */
 public class DBConnector extends DB{
 
-    public DBConnector() throws ClassNotFoundException, SQLException {
-        Class.forName(driver);
-        conn = DriverManager.getConnection(URL+db, dbuser, dbpass);
+    public DBConnector() throws ClassNotFoundException, SQLException{
+        Class.forName(super.driver);
+        super.con = DriverManager.getConnection(URL+db, dbuser,dbpass);
+    }    
+
+    public Connection connection() {
+        return this.con;
     }
 
-    DBConnector(Connection openConnection) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    public Connection openConnection(){
-        return this.conn;
-    }
-    
-    public void closeConnection() throws SQLException {
-        this.conn.close();
-    }
+    public void closeConnection() throws SQLException{
+        this.con.close();
+    }    
 }
